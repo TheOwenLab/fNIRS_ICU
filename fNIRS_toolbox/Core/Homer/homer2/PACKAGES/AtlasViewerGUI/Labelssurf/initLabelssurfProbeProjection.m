8b8159aa1b8a144aa3533e85408f5a2d915b1050
AndrouAbdalmalak <aabdalma@uwo.ca>
2024-08-04 11:47:44 -0400

    Delete fNIRS_toolbox/Core/Homer/homer2/PACKAGES/AtlasViewerGUI directory

--- fNIRS_toolbox/Core/Homer/homer2/PACKAGES/AtlasViewerGUI/Labelssurf/initLabelssurfProbeProjection.m
+++
@@ -1,9 +0,0 @@
-function labelssurf = initLabelssurfProbeProjection(labelssurf)
-
-hLabelsSurf    = labelssurf.handles.surf;
-idxL           = labelssurf.idxL;
-colormaps      = labelssurf.colormaps;
-idxCm          = labelssurf.colormapsIdx;
-
-cm = colormaps(idxCm).col;
-set(hLabelsSurf,'faceVertexCData',cm(idxL,:));
--- fNIRS_toolbox/Core/Homer/homer2/PACKAGES/AtlasViewerGUI/Pialvol/savePialvolOrient.m
+++
@@ -1,7 +0,0 @@
-function headvol = saveHeadvolOrient(headvol, obj)
-
-headvol.orientation = obj.orientation;
-if isempty(headvol.orientationOrig)
-    headvol.orientationOrig = obj.orientation;
-end
-
